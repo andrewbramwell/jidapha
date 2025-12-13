22db8bc15c8d8371889b349d890583277f9c125e
“andrewbramwell <andy@voucherstore.io>
2025-12-13 16:16:57 +0700

    schema

--- src/_data/client.js
+++ src/_data/client.js
@@ -21,6 +21,50 @@ module.exports = {
         "googleReview": "https://g.page/r/CYMYLxp1fzfHEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://jidaphas-spa.com"
+    "domain": "https://jidaphas-spa.com",
+    "schema": {
+        "business": {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "@id": "https://jidaphas-spa.com/#business",
+            "name": "Jidpha's Spa Treatments & Therapies",
+            "image": "https://jidaphas-spa.com/assets/svgs/logo-pink2.svg",
+            "url": "https://jidaphas-spa.com",
+            "telephone": "+441903261738",
+            "email": "jidaphaspatreatment@gmail.com",
+            "priceRange": "££",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "99A Ashacre Ln",
+                "addressLocality": "Worthing",
+                "postalCode": "BN13 2DE",
+                "addressCountry": "GB"
+            },
+            "geo": {
+                "@type": "GeoCoordinates",
+                "url": "https://maps.app.goo.gl/xB4Gn6y5odCiDDcf8"
+            },
+            "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+                "opens": "10:00",
+                "closes": "18:00"
+            },
+            "sameAs": [
+                "https://www.facebook.com/profile.php?id=100057380593008",
+                "https://g.page/r/CYMYLxp1fzfHEBM/review"
+            ]
+        },
+        "website": {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://jidaphas-spa.com/#website",
+            "url": "https://jidaphas-spa.com",
+            "name": "Jidpha's Spa Treatments & Therapies",
+            "publisher": {
+                "@id": "https://jidaphas-spa.com/#business"
+            }
+        }
+    }
 };
 
